ce7483c940309ba38a6a36c89fd6246a9f3220b8
sj <db970406@naver.com>
2022-06-13 22:06:52 +0900

    feat: Add search

---
+++ assets/js/components/employee/search.js
@@ -0,0 +1,21 @@
+import { showEmployeeDetailBySearch } from '../../shared/insertData.js';
+import { makeElementTag, makeIconTag, makeInputTag } from '../../shared/makeHtmlTag.js';
+
+export const searchEmployeeComponent = () => {
+    const searchEmployeeBox = makeElementTag({
+        id: "search_employee_box",
+        element: "form",
+    });
+    const searchInput = makeInputTag({
+        id: "search_employee_input",
+        placeholder: "이름"
+    });
+    searchInput.autocomplete = "off";
+    const searchIcon = makeIconTag({
+        iconName: "fas fa-search"
+    });
+
+    searchEmployeeBox.append(searchIcon, searchInput);
+    searchEmployeeBox.addEventListener("submit", showEmployeeDetailBySearch);
+    return searchEmployeeBox;
+}
