992a86f667e392062963d143fa1256b0d72f7b34
sj <db970406@naver.com>
2022-07-14 10:53:46 +0900

    feat: Add layout and index js

---
+++ assets/js/layout/layout.js
@@ -0,0 +1,6 @@
+import "../glass/categories/categories.js";
+import "../glass/dashboard/dashboard.js";
+import "../glass/weather-realtime.js";
+import "../shared/darkMode.js";
+import "../shared/webSocket.js";
+import "../glass/dashboard/notion.js";
